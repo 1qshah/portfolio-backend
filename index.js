96219751b44fd17df86ab10304048bf220d1f5ef
Q shah <1qshah@protonmail.com>
2026-08-08 18:07:20 +0000

    created backend and connected to mongodb

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const express = require('express')
+
+const Project = require('./models/Project')
+
+const app = express()
+
+app.get('/', (request, response) => {
+  Project.find({}).then(projects => {
+    response.json(projects)
+  })
+})
+
+app.get('/api/projects', (request, response) => {
+  Project.find({}).then(projects => {
+    response.json(projects)
+  })
+})
+
+app.get('/api/projects/:id', (request, response) => {
+  Project.findById(request.params.id).then(project => {
+    response.json(project)
+  })
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
